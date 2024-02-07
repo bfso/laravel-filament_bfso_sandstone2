c2cbf0421f49279a172e5082d39136fe176b7497
lionel <lionel>
2024-02-07 16:41:08 +0100

    add - database : added tables

--- database/migrations/2024_02_07_135304_create_requests_table.php
+++ database/migrations/2024_02_07_135304_create_requests_table.php
@@ -11,10 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+        Schema::create('requests', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->string('role');
+            $table->timestamps();
         });
     }
 
@@ -23,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('password_reset_tokens');
+        Schema::dropIfExists('requests');
     }
 };
--- database/migrations/2024_02_07_135557_create_tasks_table.php
+++ database/migrations/2024_02_07_135557_create_tasks_table.php
@@ -11,14 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('failed_jobs', function (Blueprint $table) {
+        Schema::create('tasks', function (Blueprint $table) {
             $table->id();
-            $table->string('uuid')->unique();
-            $table->text('connection');
-            $table->text('queue');
-            $table->longText('payload');
-            $table->longText('exception');
-            $table->timestamp('failed_at')->useCurrent();
+            $table->timestamps();
+            $table->string('title');
+            $table->string('description');
+            $table->timestamp('creation_date')->nullable();
+            $table->string('email');
         });
     }
 
@@ -27,6 +26,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('failed_jobs');
+        Schema::dropIfExists('tasks');
     }
 };
