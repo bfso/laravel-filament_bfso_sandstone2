ad35c2d0131139b287d22c02b2f8bcf653ff4250
RayenGraf <grafrayen0@gmail.com>
2024-02-07 15:11:12 +0000

    add mail template for task due

--- app/Http/Controllers/MailContainer.php
+++ app/Http/Controllers/MailContainer.php
@@ -3,8 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use \App\Mail\ActivitiesDue;
 
 class MailContainer extends Controller
 {
-    //
+    public function sendMail()
+    {
+       Mail::to("test@test.com")->send(new ActivitiesDue());
+       return "send Mail";
+    }
 }
---
+++ resources/views/mail/activitiesDue.blade.php
@@ -0,0 +1,6 @@
+<div>
+    <h1>Tätigkeit Fällig</h1>
+    <p>Aufgabe 1; muss noch gemacht werden</p>
+    <p>Aufgabe 2; muss noch gemacht werden</p>
+    <p>Aufgabe 3; muss noch gemacht werden</p>
+</div>
