15a83e0c537f6e73aa87cb1bf35c0f0c21e9a836
Jakob & Ewan <MY_NAME@example.com>
2024-02-07 14:41:19 +0000

    enhance -glob : migration

--- database/migrations/2024_02_07_143133_add_attribute_department.php
+++ database/migrations/2024_02_07_143133_add_attribute_department.php
@@ -11,10 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('deparments', function (Blueprint $table) {
-            $table->id();
-            $table->string('name');
-            $table->timestamps();
+        Schema::table('users', function ($table) {
+
+            $table->integer('department')->references('id')->on('departments');
         });
     }
 
@@ -23,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('deparments');
+        Schema::table('users', function ($table) {
+
+            $table->dropColumn('department');
+        });
     }
 };
