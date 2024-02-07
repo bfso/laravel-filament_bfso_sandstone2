e6ee4462cbeffaf318d639310add262f391c36e1
Jonas Pfammatter <joni.pfammatter@gmail.com>
2024-02-07 15:49:24 +0000

    template filament

---
+++ app/Filament/Resources/ResourceResource/Pages/CreateResource.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ResourceResource\Pages;
+
+use App\Filament\Resources\ResourceResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateResource extends CreateRecord
+{
+    protected static string $resource = ResourceResource::class;
+}
