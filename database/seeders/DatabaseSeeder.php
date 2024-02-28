e9761583533a4a88afa6654dfbae4eb67726a0b8
RayenGraf <100278450+RayenGraf@users.noreply.github.com>
2024-02-28 14:30:12 +0000

    add seeds

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Database\Seeders;
+use Illuminate\Support\Facades\Hash;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -12,11 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        \App\Models\deparment::create([
+            'name' => 'IT'
+        ]);
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        $user = \App\Models\User::create([
+            'email'=>'admin@bfo.ch',
+            'name'=>'admin@bfo.ch',
+            'password'=>Hash::make('bfo12345'),
+            'department'=>1,
+        ]);
     }
 }
