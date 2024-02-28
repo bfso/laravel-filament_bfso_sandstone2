ff0269271c1f952e1144690a3d6216c6c788c99d
RayenGraf <100278450+RayenGraf@users.noreply.github.com>
2024-02-28 13:32:47 +0000

    remove useless code implement mails #7

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,64 +10,6 @@ use Illuminate\Support\Facades\DB;
 
 class MailController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Mail $mail)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(Mail $mail)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
-    public function update(Request $request, Mail $mail)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Mail $mail)
-    {
-        //
-    }
-    
-
-
     public static function sendMail()
     {
         // get all Users
