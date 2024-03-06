c5ef13438e71be74d105d0313bc8d60a5ef0e1f8
uran <uran>
2024-03-06 15:47:45 +0000

    add - request: ui (create doesnt work)

---
+++ app/Filament/Resources/RequestResource/Pages/CreateRequest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\RequestResource\Pages;
+
+use App\Filament\Resources\RequestResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateRequest extends CreateRecord
+{
+    protected static string $resource = RequestResource::class;
+}
